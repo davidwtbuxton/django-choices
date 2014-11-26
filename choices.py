dfe45a17073785e27e748797c05ac18da79e1079
David Buxton <david@gasmark6.com>
2014-11-26 16:56:28 +0000

    Alternate implementation of Choices.
    This version uses 2 tuples to store the choices up front, but implements the same API.
    (Almost - this version raises ValueError if you use a reserved name when creating the
    choices.)

--- choices.py
+++ choices.py
@@ -1,40 +1,45 @@
-from collections import OrderedDict
-
-
-#TODO: can we just sub-class tuple and be a tuple of tuples with attributes?
 class Choices(object):
-    """ An enum-type class for creating immutable choices for django CharFields.
-        Usage:
-        MY_CHOICES = Choices([('YES', 'Yes'), ('NO', 'No'), ('MAYBE', 'Maybe')])
-        CharField(choices=MY_CHOICES.choices, default=MY_CHOICES.YES)
-        """
+    """Helper for storing enumerable Django choices. Takes a sequence of
+    (name, label) pairs. Idea copied from https://github.com/adamalton/django-choices
 
-    def __init__(self, choices):
-        super(Choices, self).__setattr__('_choices', OrderedDict(choices))
-        assert 'choices' not in self._choices.keys()
-        assert 'constants' not in self._choices.keys()
+    >>> status = c.Choices([('new', 'new'), ('complete', 'code complete')])
+    >>> status.new, status.complete
+    ('new', 'complete')
+    >>> status.new == 'new'
+    True
+    >>> status.choices
+    (('new', 'new'), ('complete', 'code complete'))
+    >>> status.finished = 'finished'
+    Traceback (most recent call last):
+        ...
+    AttributeError: 'Choices' object does not permit assigning attribute 'finished'
+    """
+    def __init__(self, pairs):
+        _choices = tuple(pairs)
+        _names = tuple(name for name, value in _choices)
 
-    def __getattr__(self, name):
-        try:
-            self._choices[name] #check it exists
-            return name
-        except KeyError:
-            return super(Choices, self).__getattr__(name)
+        self.__dict__['_choices'] = _choices
+        self.__dict__['_names'] = _names
+
+        for name in _names:
+            if hasattr(self, name):
+                raise ValueError('Cannot use reserved name %r' % name)
+
+        for name in _names:
+            self.__dict__[name] = name
 
     def __setattr__(self, name, value):
-        """ Prevent values being changed. """
-        raise AttributeError("You cannot modify attributes on a %s" % self.__class__.__name__)
+        message = '%r object does not permit assigning attribute %r'
+        raise AttributeError(message % (self.__class__.__name__, name))
 
     def __iter__(self):
-        return iter(self.constants)
-
-    def __repr__(self):
-        return unicode(self.choices)
-
-    @property
-    def constants(self):
-        return self._choices.keys() #TODO: can we make this faster by storing a constant reference?
+        return iter(self._names)
 
     @property
     def choices(self):
-        return self._choices.items() #TODO: can we make this faster by storing a constant reference?
+        return self._choices
+
+    @property
+    def constants(self):
+        return self._names
+
